64859275fac2dca4d4e8ac4209cd15e5f66532a1
wowow <wowow.908@gmail.com>
2021-11-08 10:19:34 +0700

    seri belajar component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  name: string = "Ucup";
+
+  displayTitle = false;
+
+  eventClick(){
+    this.displayTitle = !this.displayTitle;
+  }
+
+  items = [
+    "item1",
+  ]
+
+  addItem(){
+    this.items.push(`item${(this.items.length+1)}`);
+  }
+
+  // cunstructor adalah method yang pertama kali dijalankan dalam angular
   constructor() { }
 
+  // ngOnInit adalah method yang kedua yang dijalankan dalam angular
+  // penggunaan ngOnInit harus diimplement dalam class
   ngOnInit(): void {
+    setTimeout(()=>{
+      this.getData();
+    }, 2000);
+  }
+
+  // fetch api
+  getData(){
+    alert("Get data success!")
   }
 
 }
